1e949bc26b3bf8001b1914fbc8f96fca804b6a6e
hstefanova <hristiyana.stefanova@mentormate.com>
2023-02-03 13:45:50 +0200

    small fix

--- app/src/components/User.js
+++ app/src/components/User.js
@@ -1,7 +1,7 @@
 import styles from './UserList.module.css';
 
 function User(props) {
-  return <li className={styles['user__row']}>Test: {props.user.name}</li>;
+  return <li className={styles['user__row']}>{props.user.name}</li>;
 }
 
 export default User;
